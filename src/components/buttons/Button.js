a52c9fa0b176d00648c826d2100deb46121f1288
John Paul San Diego <johnpaulsandiego@Johns-MBP.communityfibre.co.uk>
2024-09-07 22:22:12 +0100

    More imports

--- src/components/buttons/Button.js
+++ src/components/buttons/Button.js
@@ -1,13 +1,13 @@
 import React from 'react';
 
 const Button = ({ type = 'a', label, onClick }) => {
-  const baseClasses = 'font-semibold text-light-grey text-sm px-4 py-2 min-w-[60px] mx-1 rounded-md outline-white cursor-pointer transition-colors duration-300';
+  const baseClasses = 'font-titillium font-semibold text-sm px-4 py-2 min-w-[60px] mx-1 rounded-md cursor-pointer transition-all ease-in-out duration-300';
   const typeClasses = {
-    a: 'bg-transparent text-white border border-white hover:bg-white/10',
+    a: 'bg-transparent text-white border border-white hover:bg-white/10', // Updated to use bg-white/10 for hover effect
     b: 'bg-transparent text-black border border-transparent hover:bg-black/10',
     c: 'bg-accent-color-a text-white border-none hover:bg-accent-color-a/80',
   };
-  
+
   return (
     <button className={`${baseClasses} ${typeClasses[type]}`} onClick={onClick}>
       {label}
