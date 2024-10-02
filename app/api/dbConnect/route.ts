b96810fdb360fe7177d47661b871cef184278edc
Waseef Mohammad Khan <waseef2010@gmail.com>
2024-10-02 18:17:32 +0100

    api

--- app/api/dbConnect/route.ts
+++ app/api/dbConnect/route.ts
@@ -3,10 +3,15 @@ import mysql from "mysql2/promise";
 
 export async function GET() {
   // Define the database connection details using types
-  const host_name: string = process.env.NEXT_PUBLIC_DB_HOST!;
-  const database: string = process.env.NEXT_PUBLIC_DB_NAME!;
-  const user_name: string = process.env.NEXT_PUBLIC_DB_USER!;
-  const password: string = process.env.NEXT_PUBLIC_DB_PASSWORD!;
+  const host_name: string = process.env.DB_HOST!;
+  const database: string = process.env.DB_NAME!;
+  const user_name: string = process.env.DB_USER!;
+  const password: string = process.env.DB_PASSWORD!;
+
+  console.log("host_name: ", host_name);
+  console.log("database: ", database);
+  console.log("user_name: ", user_name);
+  console.log("password: ", password);
 
   try {
     // Create a MySQL connection using the mysql2/promise library with TypeScript
@@ -15,6 +20,7 @@ export async function GET() {
       user: user_name,
       password: password,
       database: database,
+      port: 3306,
     });
 
     // Execute a simple query to test the connection
